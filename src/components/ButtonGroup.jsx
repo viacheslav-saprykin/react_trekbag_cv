2b47e21837b6b13c3b55f1ee9c75d8ed577ef06d
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-05-19 18:32:34 +0300

    fixed: group buttons

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,20 @@
 import Button from "./Button";
 
 export default function ButtonGroup() {
+  const secondaryButtons = [
+    "Mark all as complete",
+    "Mark all as incomplete",
+    "Reset to initial",
+    "Remove all items"
+  ];
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {secondaryButtons.map((text) => (
+        <Button key={text} type="secondary">
+          {text}
+        </Button>
+      ))}
+
     </section>
   )
 }
